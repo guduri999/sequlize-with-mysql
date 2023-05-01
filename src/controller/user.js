98e9d52089c88353b45523bde8d0f7781a56ffe1
restaurent999 <gudurisandeep999@gmail.com>
2023-05-01 14:35:10 +0530

    express.json()

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,7 +1,18 @@
-
+import { UsersTable } from "../modal/index.js"
 
 export const getAllUsers = (req, res) => {
-    res.status(200).json({ hello: "getAllUsersire" })
+    // const val = UsersTable.create({
+    //     userName: 'he;;p',
+    //     Password: 'asdf',
+    //     Email: 'asfd',
+    //     FirstName: 'asdf',
+    //     LastName: 'asfd',
+    //     Gender: 'asf',
+    //     DateOFBorth: 'asf',
+    //     Address: 'asf',
+    //     PhoneNumber: 'asfd',
+    // })
+    res.status(200).json({ hello: "" })
 }
 export const getUserById = (req, res) => {
     res.status(200).json({ hello: "getUserById" })

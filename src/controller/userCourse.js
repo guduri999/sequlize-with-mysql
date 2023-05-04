e95ee7e32b192928d40203561fc7d6b96953098c
restaurent999 <gudurisandeep999@gmail.com>
2023-05-04 13:39:19 +0530

    code cleaning

--- src/controller/userCourse.js
+++ src/controller/userCourse.js
@@ -1,7 +1,7 @@
 import { userCoursesTable } from "../modal/index.js";
 
 export const enrollCourse = async (req, res) => {
-    console.clear()
+
     await userCoursesTable.findOrCreate({
         where: {
             userId: req.body.userId,
@@ -11,6 +11,6 @@ export const enrollCourse = async (req, res) => {
             CoursesId: req.body.CoursesId
         }
     })
-        .then(data => data[1] ? res.status(200).json({ response: "successfully created", data }) : res.status(200).json({ response: "already craated", data }))
-        .catch(error => console.log(`getting error ${error}`))
+        .then(data => data[1] ? res.status(200).json({ response: "successfully created", data }) : res.status(200).json({ response: "already created", data }))
+        .catch(error => res.status(401).json({ response: "getting error", error }))
 }
